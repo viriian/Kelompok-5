c73ab16b3b958fa3db3ebdb1a4b633f2f334f29b
Nurhasnh <114034337+Nurhasnh@users.noreply.github.com>
2024-07-11 13:08:26 +0700

    Update dashboard_admin.php

--- ARSLib/dashboard_admin.php
+++ ARSLib/dashboard_admin.php
@@ -13,7 +13,6 @@ $availableBooks = $totalBooks - $borrowedBooks;  // Sisa buku yang tersedia
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Admin</title>
-    <!-- Load Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="dashboard_admin.css">
 </head>
